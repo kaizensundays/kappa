f627391cea328a55ba4eec9da4652ffe8c0c91a1
KaizenSundays <kaizensundays@localhost>
2023-12-03 17:46:42 -0500

    [ kappa ]  JDK ProcessBuilder

---
+++ kappa/kappa-service/src/main/java/com/kaizensundays/fusion/kappa/os/JDKProcessWrapper.kt
@@ -0,0 +1,33 @@
+package com.kaizensundays.fusion.kappa.os
+
+import com.kaizensundays.fusion.kappa.unsupportedOperation
+import java.util.concurrent.TimeUnit
+
+/**
+ * Created: Sunday 12/3/2023, 5:38 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+class JDKProcessWrapper(private val process: Process) : KappaProcess {
+
+    override fun pid(): Int {
+        return process.pid().toInt()
+    }
+
+    override fun isRunning(): Boolean {
+        return process.isAlive
+    }
+
+    override fun shutdown(timeout: Long, timeUnit: TimeUnit) {
+        unsupportedOperation()
+    }
+
+    override fun waitFor(timeout: Long, timeUnit: TimeUnit): Int {
+        return if (process.waitFor(timeout, timeUnit)) process.exitValue() else -1
+    }
+
+    override fun destroy(force: Boolean) {
+        process.destroy()
+    }
+
+}
