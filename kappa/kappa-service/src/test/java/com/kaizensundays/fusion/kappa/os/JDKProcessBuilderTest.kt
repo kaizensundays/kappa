acd0a9759cae609c7d13e596aabb823749334723
KaizenSundays <kaizensundays@localhost>
2023-12-09 18:37:15 -0500

    [ kappa ] ProcessBuilder

--- kappa/kappa-service/src/test/java/com/kaizensundays/fusion/kappa/os/JDKProcessBuilderTest.kt
+++ kappa/kappa-service/src/test/java/com/kaizensundays/fusion/kappa/os/JDKProcessBuilderTest.kt
@@ -1,8 +1,12 @@
 package com.kaizensundays.fusion.kappa.os
 
 import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.util.concurrent.TimeUnit
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 /**
  * Created: Saturday 12/9/2023, 6:17 PM Eastern Time
@@ -12,6 +16,31 @@ import kotlin.test.assertEquals
 class JDKProcessBuilderTest : OsTestSupport() {
 
     private val builder = JDKProcessBuilder()
+
+    @Test
+    fun startProcessWithJDK() {
+
+        val command = command()
+
+        val process = ProcessBuilder()
+            .command(command)
+            .start()
+
+        val inputStream: InputStream = process.inputStream
+        val reader = BufferedReader(InputStreamReader(inputStream))
+
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            println(line)
+        }
+
+        //val exitCode: Int = process.waitFor()
+        val started = process.waitFor(30, TimeUnit.SECONDS)
+        assertTrue(started)
+        assertEquals(0, process.exitValue())
+
+        Thread.sleep(3_000)
+    }
 
     @Test
     fun startProcess() {
--- kappa/kappa-service/src/test/java/com/kaizensundays/fusion/kappa/os/KappaNuProcessBuilderTest.kt
+++ kappa/kappa-service/src/test/java/com/kaizensundays/fusion/kappa/os/KappaNuProcessBuilderTest.kt
@@ -1,9 +1,6 @@
 package com.kaizensundays.fusion.kappa.os
 
 import org.junit.jupiter.api.Test
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
 import java.lang.Thread.sleep
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -17,7 +14,7 @@ import kotlin.test.assertTrue
  *
  * @author Sergey Chuykov
  */
-class KappaNuProcessBuilderTest {
+class KappaNuProcessBuilderTest : OsTestSupport() {
 
     private val builder = KappaNuProcessBuilder()
 
@@ -34,56 +31,8 @@ class KappaNuProcessBuilderTest {
         assertFalse(builder.isWindows(props))
     }
 
-    fun command(): List<String> {
-        return if (builder.isWindows(System.getProperties()))
-            listOf("cmd", "/c", "dir", "*.xml")
-        else
-            listOf("/bin/bash", "-c", "ls *.xml")
-    }
-
     @Test
-    fun startProcessWithJDK() {
-
-        val command = command()
-
-        val process = ProcessBuilder()
-            .command(command)
-            .start()
-
-        val inputStream: InputStream = process.inputStream
-        val reader = BufferedReader(InputStreamReader(inputStream))
-
-        var line: String?
-        while (reader.readLine().also { line = it } != null) {
-            println(line)
-        }
-
-        //val exitCode: Int = process.waitFor()
-        val started = process.waitFor(30, TimeUnit.SECONDS)
-        assertTrue(started)
-        assertEquals(0, process.exitValue())
-
-        sleep(3_000)
-    }
-
-    @Test
-    fun startWithJdk() {
-
-        val command = command()
-
-        val process = builder.setCommand(command)
-            .setJdk(true)
-            .start()
-
-        val exitCode = process.waitFor(10, TimeUnit.SECONDS)
-
-        assertEquals(0, exitCode)
-
-        sleep(3_000)
-    }
-
-    @Test
-    fun startWithNu() {
+    fun startProcess() {
 
         val command = command()
 
