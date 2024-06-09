b7ec07c2e15c330d667b40ebbcd4a0406a018180
KaizenSundays <kaizensundays@localhost>
2024-06-09 13:33:30 -0400

    Kapplet

--- kappa/kappa-node/src/main/java/com/kaizensundays/kappa/KappletContext.kt
+++ kappa/kappa-node/src/main/java/com/kaizensundays/kappa/KappletContext.kt
@@ -1,23 +1,8 @@
 package com.kaizensundays.kappa
 
-import com.kaizensundays.fusion.kappa.KappletKtorServer
-import com.kaizensundays.fusion.kappa.core.api.cast
-import com.kaizensundays.fusion.kappa.core.api.Handler
-import com.kaizensundays.fusion.kappa.core.api.Request
-import com.kaizensundays.fusion.kappa.core.api.Service
-import com.kaizensundays.fusion.kappa.core.api.isWindows
-import com.kaizensundays.fusion.kappa.os.Linux
-import com.kaizensundays.fusion.kappa.os.NuProcessBuilderImpl
-import com.kaizensundays.fusion.kappa.os.Os
-import com.kaizensundays.fusion.kappa.os.Windows
-import com.kaizensundays.fusion.kappa.service.Kapplet
-import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportResource
 import org.springframework.context.annotation.PropertySource
-import javax.cache.Cache
 
 /**
  * Created: Saturday 9/10/2022, 1:17 PM Eastern Time
@@ -28,11 +13,5 @@ import javax.cache.Cache
 @PropertySource("classpath:/application.properties")
 @ImportResource("classpath:KappletContextConfig.xml", "classpath:KappletContext.xml")
 open class KappletContext {
-
-    @Value("\${kapplet.server.port}")
-    var serverPort = 0
-
-    @Bean
-    open fun ktorServer(os: Os, kapplet: Kapplet) = KappletKtorServer(serverPort, os, kapplet)
 
 }
