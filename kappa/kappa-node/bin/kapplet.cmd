a8962f0758b23d4be7b4bc14b75d32cc5bde7905
KaizenSundays <kaizensundays@localhost>
2024-06-21 21:31:33 -0400

    [ kappa ] POM cleanup

--- kappa/kappa-node/bin/kapplet.cmd
+++ kappa/kappa-node/bin/kapplet.cmd
@@ -1,6 +1,10 @@
-
-start "Kapplet" %JAVA_11_HOME%/bin/java -Xmx256m ^
-	-Dlog4j2.configurationFile=classpath:kapplet-log4j2.xml ^
-        -Dlog4j2.debug=false ^
+start "Kapplet" %JAVA_17_HOME%/bin/java -Xmx256m ^
+	-Dproperties=kapplet.yml ^
+	-Dlog4j.configurationFile=classpath:kapplet-log4j2.xml ^
+	-Dlog4j2.debug=false ^
 	-Dlog4j2.shutdownHookEnabled=false ^
+	--add-opens=java.base/sun.nio.ch=ALL-UNNAMED ^
+	--add-opens=java.base/java.lang.invoke=ALL-UNNAMED ^
+	--add-opens=java.base/java.nio=ALL-UNNAMED ^
+	--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED ^
 	-jar kappa.jar
