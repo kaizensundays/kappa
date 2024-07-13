9fb72ca00b964805a1955e9c72f1b8424139db46
KaizenSundays <kaizensundays@localhost>
2024-07-13 12:39:06 -0400

    [ kappax ] static

---
+++ kappax/kappa-service/src/main/java/com/kaizensundays/fusion/kappa/KappletWebServer.kt
@@ -0,0 +1,61 @@
+package com.kaizensundays.fusion.kappa
+
+import io.ktor.server.application.call
+import io.ktor.server.cio.CIO
+import io.ktor.server.engine.ApplicationEngine
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.http.content.defaultResource
+import io.ktor.server.http.content.resource
+import io.ktor.server.http.content.resources
+import io.ktor.server.http.content.static
+import io.ktor.server.http.content.staticBasePackage
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * Created: Saturday 7/13/2024, 12:13 PM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+class KappletWebServer(
+    private val port: Int
+) {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
+
+    private var engine: ApplicationEngine? = null
+
+    private fun startServer() {
+        embeddedServer(CIO, port = this.port, configure = {
+        }) {
+            routing {
+                static("/") {
+                    staticBasePackage = "static"
+                    resource("index.html")
+                    defaultResource("index.html")
+                }
+/*
+                get("/ping") {
+                    call.respondText("Web: Ok")
+                }
+*/
+            }
+        }.start(wait = true)
+    }
+
+    fun start() {
+        Thread({ startServer() }, "web-start").start()
+
+        logger.info("Started")
+    }
+
+    fun stop() {
+        engine?.stop(3000L, 7000L)
+
+        logger.info("Stopped")
+    }
+
+}
