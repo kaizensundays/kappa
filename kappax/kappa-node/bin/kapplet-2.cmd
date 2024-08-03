1acf8ca547020780be45f4800937f5cf184ed1f3
KaizenSundays <kaizensundays@localhost>
2024-08-03 12:25:56 -0400

    [ kappax ] cluster

---
+++ kappax/kappa-node/bin/kapplet-2.cmd
@@ -0,0 +1,10 @@
+start "Kapplet" %JAVA_17_HOME%/bin/java -Xmx256m ^
+	-Dproperties=kapplet-2.yml ^
+	-Dlog4j.configurationFile=file:kapplet-log4j2-2.xml ^
+	-Dlog4j2.debug=false ^
+	-Dlog4j2.shutdownHookEnabled=false ^
+	--add-opens=java.base/sun.nio.ch=ALL-UNNAMED ^
+	--add-opens=java.base/java.lang.invoke=ALL-UNNAMED ^
+	--add-opens=java.base/java.nio=ALL-UNNAMED ^
+	--add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED ^
+	-jar kappa.jar
