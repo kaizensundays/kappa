bbe9a38b80b128b031aceddc0cdff1cf96de7acc
KaizenSundays <kaizensundays@localhost>
2024-08-03 13:10:16 -0400

    [ kappax ] Dockerfile

--- kappax/kappa-node/bin/kapplet-0.cmd
+++ kappax/kappa-node/bin/kapplet-0.cmd
@@ -1,5 +1,18 @@
+
+#
+# Single node cluster
+#
+
+set ATOMIX_PROFILE=Consensus
+set ATOMIX_BOOTSTRAP=SINGLE:localhost:5501
+set ATOMIX_NODE_PORT=5501
+set ATOMIX_NODE_ID=SINGLE
+
+set KAPPLET_SERVER_PORT=7701
+set KAPPLET_WEB_PORT=7703
+
 start "Kapplet" %JAVA_17_HOME%/bin/java -Xmx256m ^
-	-Dproperties=kapplet-0.yml ^
+	-Dproperties=kapplet.yml ^
 	-Dlog4j.configurationFile=file:kapplet-log4j2-0.xml ^
 	-Dlog4j2.debug=false ^
 	-Dlog4j2.shutdownHookEnabled=false ^
--- kappax/kappa-node/bin/kapplet.cmd
+++ kappax/kappa-node/bin/kapplet.cmd
@@ -1,3 +1,17 @@
+
+#
+# Single node DataGrid
+#
+
+set ATOMIX_PROFILE=DataGrid
+set ATOMIX_BOOTSTRAP=DG:localhost:5501
+set ATOMIX_NODE_PORT=5501
+set ATOMIX_NODE_ID=DG
+
+set KAPPLET_SERVER_PORT=7701
+set KAPPLET_WEB_PORT=7703
+
+
 start "Kapplet" %JAVA_17_HOME%/bin/java -Xmx256m ^
 	-Dproperties=kapplet.yml ^
 	-Dlog4j.configurationFile=classpath:kapplet-log4j2.xml ^
